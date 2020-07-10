84562eb1dd498b86ec5ea96e496f57e4e1e11311
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-10 13:27:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,8 +22,10 @@ class InteractiveRecord
     column_names.compact
   end
 
-  self.column_names.each { |col_name| attr_accessor col_name.to_sym }
-
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+  
   def initialize(options={})
     options.each {|property, value| self.send("#{property}=", value)}
   end
